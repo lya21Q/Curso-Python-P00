57b5d83fd9734be526bdeae5b900190ea307c4ed
rosalinda <perezvelayuo@gmail.com>
2025-03-11 16:00:48 -0600

    clase

--- Clases_y_objetos/atributos-de-distancia.py
+++ Clases_y_objetos/atributos-de-distancia.py
@@ -11,18 +11,32 @@ class   Estudiante:
 
 
 class   Profesor:
-    def __init__(self,nombre):
+    def __init__(self,nombre,temas_dominados:list[str]):
         self.nombre=nombre
-        self.temas_dominados=[]
+        self.temas_dominados=temas_dominados
     def dominar_temas(self,tema:str)->None:
         self.temas_dominados.append(tema)
     def ensenar_tema(self, no_tema:int)->str:
-        if no_tema >len(self.temas_dominados):
+        if no_tema < len(self.temas_dominados):
             return self.temas_dominados[no_tema]
         else:
             return "Fuera de rango"
     def __str__(self)->str:
         return f"Profesor(nombre:{self.nombre},temas dominados : {self.temas_dominados})"
+
+class Empleado:
+   # __no_id__: int  # Es como un contador
+    no_id=1
+    def __init__(self,nombre,sueldo):
+        self.nombre=nombre
+        self.sueldo=sueldo
+        self.id=Empleado.no_id
+        Empleado.no_id += 1
+    def aumentar_sueldo(self,porcentaje:float)->None:
+        pass
+    def __str__(self)-> str:
+        Empleado(id={self.id},nombre={self.nombre})
+
 if __name__=='__main__':
     estudiante1=Estudiante("Rosalinda Aquino Pérez")
     estudiante2=Estudiante("Jennifer Gutierrez")
@@ -31,9 +45,19 @@ if __name__=='__main__':
     print(estudiante1)
     print(estudiante2)
 
-    profe=Profesor("Alberto Martínez Barbosa")
-    profe.dominar_temas("Atributos de distancia")
+    profe=Profesor("Alberto Martínez Barbosa", ["Atributos de distancia, instructor de musica, recursividad"])
+    #tema1=Profesor.ensenar_tema(1)
+    #tema2=Profesor.ensenar_tema(2)
+    #estudiante1.aprender_tema(tema1)
+    #estudiante2.aprender_tema(tema1)
+    #estudiante2.aprender_tema(Profesor.ensenar_tema(tema2))
+    #profe.dominar_temas("Atributos de distancia")
+    #profe.dominar_temas("Instructor de basquetbol")
     print(profe)
+
+    empleado1=Empleado("Jose Ramirez", 250)
+    empleado2=Empleado("Maria Ramirez", 250)
+    print(Empleado.no_id)
 
 
 
