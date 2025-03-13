f52622ed3af3787e6a2d07db109aacddaee50cca
rosalinda <perezvelayuo@gmail.com>
2025-03-13 16:02:44 -0600

    clase

--- Clases_y_objetos/personaje.py
+++ Clases_y_objetos/personaje.py
@@ -1,28 +1,41 @@
 class Personaje:
-    no_id=1
-    def __init__(self, x :int, y: int):
-        self.x=x
-        self.y=y
+    #Clase que representa a un personaje.
+    #SUS ATRIBUTOS SON, X y Y.
+    contador_id:int=1
+    no_id = 1
+    def __init__(self):
+        #Atributos de instancia
+        self.X=0
+        self.Y=0
+
+        #Atributo independiente
         self.id_personaje=Personaje.no_id
         Personaje.no_id+=1
 
-    def moverse(self)->None:
-        for i in range(1,10):
-            if i in range<=(1,10):
-                print("Ingrese eñ ")
+    def moverse(self, ordenes:str)->None:
+        for palabra in ordenes:
+            if palabra == "A" or "a":
+                self.Y+=1
+            elif palabra == "R" or "r":
+                self.Y-=1
+            elif palabra == "D" or "d":
+                self.X+=1
+            elif palabra == "I" or "i":
+                self.X-=1
 
 
     def posicion_actual(self)->None:
-        pass
+        print(f"Posición actual de X:{self.X} y la posición actual de Y es: {self.Y} ")
+
 
     def __str__(self):
-        pass
+        return f"Personaje(id: {self.id_personaje}"
 
-    if __name__=="__main__":
-        pass
+    """ %%%%%%%     CÓDIGO A NIVEL DE MÓDULO    %%%%%%%%%%%%%%%%%%%%% """
+if __name__ =="__main__":
+    #Personaje=()
+    personaje1=Personaje()
+    personaje1.moverse("a")
+    personaje1.posicion_actual()
 
-    personaje1=Personaje("Ingresa las órdenes de movimiento")
-    personaje2=Personaje("")
-    #personake3=
-
-
+    while personaje1.moverse != "s":
