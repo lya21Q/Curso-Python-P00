300ec7c0a154ab5a72e6576a34d526e7f9d99350
rosalinda <perezvelayuo@gmail.com>
2025-03-20 16:09:43 -0600

    clase

--- Clases_y_objetos/encapsulamiento.py
+++ Clases_y_objetos/encapsulamiento.py
@@ -2,7 +2,7 @@
 #DIagrama de clases de UML
 class CuentaBancaria:
     def __init__(self,titular:str,saldo_inicial:float=0)->None:
-        #constructor
+    #constructor
         self.titular=titular
         self._saldo=saldo_inicial
     def depositar(self,cantidad:float)->None:
@@ -15,13 +15,19 @@ class CuentaBancaria:
         return self._saldo
 
     def retirar(self,cantidad:float)->None:
-
+        pass
+    @saldo.setter
     def saldo(self,nuevo_saldo:float)->None:
         self._saldo=nuevo_saldo
 
+    def __str__(self) -> str:
+        return f"(Titular {self.titular}, saldo inicial {self._saldo})"
 
 
 if __name__ == '__main__':
     cuenta_guadalupe=CuentaBancaria("Guadalupe",0)
     cuenta_guadalupe._saldo=10
+    print(cuenta_guadalupe)
+    #S
     cuenta_guadalupe.saldo=5
+    print(cuenta_guadalupe)
