e1797332ae67b85396cfa37bdc41cd7cf2f946a1
rosa <perezvelayuo@gmail.com>
2025-03-29 23:44:30 -0600

    terminado

---
+++ Codewars/Filtrado_de_números_pares.py
@@ -0,0 +1,11 @@
+def kata_13_december(lst):
+    i=0
+    while i < len(lst):
+        if lst[i]%2==0:
+            lst.pop(i)
+        else:
+            i+=1
+    return lst
+lst=[1, 2, 2, 2, 4, 3, 4, 5, 6, 7]
+resultado=kata_13_december(lst)
+print(f"La lista actualizada es:{lst}")
