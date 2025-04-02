3fae3e2d62b0d9591eddfeb30d1f73c9ae1ab168
rosalinda <perezvelayuo@gmail.com>
2025-04-02 16:04:26 -0600

    por terminar

--- examen/exam_par1.py
+++ examen/exam_par1.py
@@ -1,19 +1,33 @@
 class Jugador:
+    #nombre,numero,goles:Métodos de acceso para obtener y modificar atributos.
     def __init__(self,nombre:str,numero,goles:int=0) -> None:
+        """Constructor para inicializar los atributos."""
         self._nombre=nombre
         self._numero=numero
         self._goles=goles
+
     def anotar_goles(self,no_goles: int):
-        pass
+        no_goles=self._goles
+        return self._goles
+
     def __str__(self):
-        pass
+       return f"Jugador(Nombre del jugador:{self._nombre}, numero de jugador:{self._numero},cantidad de goles anotados {self._goles})"
 
 class Equipo:
+    contador_id:int=0
+    no_id = 1
     def __init__(self,nombre:str,*jugadores:tuple[Jugador]):
-        self._nombre=nombre
-        self._jugadores=jugadores
+        self._nombre=list(nombre)
+        self._jugadores=tuple[jugadores]
+
     def agregar_jugadores(self,*jugadores: tuple[Jugador]):
-        pass
+        for jugador in jugadores:
+            if jugador != self._jugadores:
+                jugadores=jugadores.append(jugador)
+            else:
+                print("El jugador ya existe")
+        return self._jugadores
+
     def remover_jugadores(self,*jugadores: tuple[Jugador]):
         pass
     def mostrar_jugadores(self):
@@ -36,3 +50,16 @@ class Torneos:
         pass
     def __str__(self):
         pass
+
+if __name__=="__main__":
+    print("Bienvenido al torneo: Champions ")
+    op=None
+    while op!=0:
+        print("[1].Crea un-")
+        print("[1].-")
+        print("[1].-")
+        print("[1].-")
+        print("[1].-")
+        print("[1].-")
+        print("[1].-")
+
