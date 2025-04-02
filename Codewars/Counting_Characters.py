bfd2cf22311c6a8fad72fb19417d2f585f32796f
rosa <perezvelayuo@gmail.com>
2025-04-01 19:00:59 -0600

    terminado

--- Codewars/Counting_Characters.py
+++ Codewars/Counting_Characters.py
@@ -1,13 +1,22 @@
 """
+Nombtre: Rosalinda Aquino Pérez
+Fecha: 01/04/2025
+Instrucciones:
 Define un méodo llamado countCharOccurrenceso, count_char_occurrencesque acepta una cadena y un carácter como
 entradas y devuelve la cantidad de veces que el carácter aparece en la cadena como un int.
 """
-strng=input("Ingrese una palabra:")
-char=input("Ingrese un caracter:")
-def count_char_occurrences(strng, char):
+
+def count_char_occurrences(strng:str, char:str)->int:
+#Parametros: strng(str):Cadena ingresada por el usuario.
+#char(str): Caracter ingresado por el usuiario.
+#Retorna : cont (int): El número que representa la cantidad de veces que el carácter aparece en la cadena.
     cont=0
     for letra in strng:
         if letra == char:
             cont+=1
     return cont
-print(count_char_occurrences(strng,char))
+
+if __name__ == "__main__":
+    strng = input("Ingrese una palabra:")
+    char = input("Ingrese un caracter:")
+    print(count_char_occurrences(strng,char))
