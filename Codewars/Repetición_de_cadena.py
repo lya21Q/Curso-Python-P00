e712884327b6229e1ebb1ff5e1e7202ed05f3ae0
rosa <perezvelayuo@gmail.com>
2025-04-01 19:31:41 -0600

    terminado

--- Codewars/Repetición_de_cadena.py
+++ Codewars/Repetición_de_cadena.py
@@ -1,11 +1,21 @@
 """
-Escriba una función que acepte un entero no negativo ny una cadena scomo parámetros, y devuelva una cadena srepetida exactamente nveces.
+Nombre: Rosalinda Aquino Pérez
+Fecha: 01/04/2025
+Instrucciones:
+Escriba una función que acepte un entero no negativo ny una cadena scomo parámetros, y devuelva una cadena s repetida exactamente n veces.
 Ejemplos (entrada -> salida)
 6, "I"     -> "IIIIII"
 5, "Hello" -> "HelloHelloHelloHelloHello"
 """
-def repeat_str(repeat, string):
-    if repeat > 0:
-        return repeat * string
-    elif repeat == 0:
-        return ''
+
+def repeat_str(repeat:int, string:str)->str:
+    #parametros:repeat(int):Representa el numero de veces que se repetira la palabra.
+    #string(str):Cadena que se repetira.
+    if repeat > 0:# Mientras el numero sea mayor que cero
+        return repeat * string#Multiplica la cadena con el número ingresado.
+    elif repeat == 0:#Si es cero
+        return ''#Retorna un cadena vacia
+if __name__ == "__main__":
+    repeat = int(input("ingrese un numero:"))
+    string = input("ingrese una palabra:")
+    print(repeat_str(repeat,string))
