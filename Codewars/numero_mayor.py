51b2abb0cf8bcafc49b942b3d7e8d615b0320c9e
rosa <perezvelayuo@gmail.com>
2025-04-02 01:04:02 -0600

    terminado

--- Codewars/numero_mayor.py
+++ Codewars/numero_mayor.py
@@ -1,13 +1,27 @@
-
-st='hi!'
-def replace_exclamation(st):
+"""
+Nombre:Rosalinda Aquino Pérez
+Fecha: 01/04/2025
+Instrucciones:
+Reemplace todas las vocales por signos de exclamación en la oración. aeiouAEIOUes vocal.
+Ejemplos
+"Hi!" --> "H!!"
+"!Hi! Hi!" --> "!H!! H!!"
+"aeiou" --> "!!!!!"
+"ABCDE" --> "!BCD!"
+"""
+# Funcion que sustutuye las vocales en una oracion por el simbolo de exclamación
+def replace_exclamation(st:str)->str:
     a='!'
     esp=""
     for letra in st:
-        if letra == 'a' or letra == 'e' or letra =="i" or letra == "o" or letra =="u":
+        if letra == 'a' or letra == 'e' or letra =="i" or letra == "o" or letra =="u":# Comprueba si la letra es una vocal, ninuscula
             esp+=a
-        elif letra == 'A' or letra == 'E' or letra =="I"or letra == "O" or letra =="U":
+        elif letra == 'A' or letra == 'E' or letra =="I"or letra == "O" or letra =="U":# Comprueba si la letra es una vocal,mayuscula.
+
             esp+=a
         else:
             esp+= letra
     return esp
+if __name__ == "__main__":
+    st = 'hi!'
+    print(replace_exclamation(st))
