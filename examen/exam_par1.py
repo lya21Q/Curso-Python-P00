ed745e7463d08080bdd61d8b7c29499856c56379
rosalinda <perezvelayuo@gmail.com>
2025-04-02 16:20:16 -0600

    por terminar

--- examen/exam_par1.py
+++ examen/exam_par1.py
@@ -13,11 +13,16 @@ class Jugador:
     def __str__(self):
        return f"Jugador(Nombre del jugador:{self._nombre}, numero de jugador:{self._numero},cantidad de goles anotados {self._goles})"
 
+_id:int
+_nombre:str
+_jugadores:list[Jugador]
+_id_equipo:int{id_equipo=_id}
+
 class Equipo:
     contador_id:int=0
     no_id = 1
     def __init__(self,nombre:str,*jugadores:tuple[Jugador]):
-        self._nombre=list(nombre)
+        self._nombre=nombre
         self._jugadores=tuple[jugadores]
 
     def agregar_jugadores(self,*jugadores: tuple[Jugador]):
@@ -36,6 +41,8 @@ class Equipo:
         pass
     def __str__(self):
         pass
+_nombre:str
+_equipos:list[Equipo]
 class Torneos:
     def __init__(self,nombre:str,*equipos:tuple[Equipo]):
         self._nombre=nombre
@@ -55,11 +62,18 @@ if __name__=="__main__":
     print("Bienvenido al torneo: Champions ")
     op=None
     while op!=0:
-        print("[1].Crea un-")
-        print("[1].-")
-        print("[1].-")
-        print("[1].-")
-        print("[1].-")
-        print("[1].-")
-        print("[1].-")
-
+        print("[1].Crea un nuevo jugador.")
+        print("[2].-Crear nuevo equipo.")
+        print("[3].-Ver lista de jugadores.")
+        print("[4].-Ver lista de equipos.")
+        print("[5].-Agregar jugadores a algún equipo.")
+        print("[6].-Eliminar jugadores de un equipo.")
+        print("[7].-Anotar goles a un jugador.")
+        print("[8].-Anotar goles a un jugador.")
+        print("[9].-Anotar goles a un jugador.")
+        print("[10].-Anotar goles a un jugador.")
+        print("[11].-Anotar goles a un jugador.")
+        print("[0].-Salir.")
+        op=int(input("Ingresa una opción"))
+        if op == 1:
+            agre
