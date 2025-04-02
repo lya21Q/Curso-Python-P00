d098e9c97b310241b1a575c0fa555f242ead80ee
rosa <perezvelayuo@gmail.com>
2025-04-02 00:59:49 -0600

    terminado

--- Codewars/else syntax debug.py
+++ Codewars/else syntax debug.py
@@ -1,4 +1,7 @@
 """
+Nombre:Rosalinda Aquino Pérez
+Fecha:01/04/2025
+Instrucciones
 Depuración de sintaxis if/else
 checkAliveMientras creaba un juego, tu compañero Greg decidió crear una función llamada // para comprobar si el usuario sigue vivo CheckAlive. check_aliveDesafortunadamente, Greg cometió algunos errores al crear la función.
 checkAlive// debe devolver verdadero si la salud CheckAlivedel check_alivejugador es mayor que 0 o falso si es 0 o menor.
@@ -6,8 +9,8 @@ La función recibe un parámetro healthque siempre será un número entero entre
 """
 
 from random import randint
-
-def check_alive(health):
+#retorna  True si la salud es mayor a 0, False de lo contrario.
+def check_alive(health:int)->bool:
     if health <= 0:
         return False
     else:
