f30c91fe773952150691bd8bd0dc7581bba37585
rosa <perezvelayuo@gmail.com>
2025-04-01 20:11:40 -0600

    terminado

--- Codewars/Sólo_necesitas_uno.py
+++ Codewars/Sólo_necesitas_uno.py
@@ -1,10 +1,19 @@
 """
-Recibirá una matriz ay un valor x. Solo necesita comprobar si la matriz proporcionada contiene el valor.
-a Puede contener números o cadenas. xPuede ser cualquiera de los dos.
-Devuelve truesi la matriz contiene el valor, falsesi no.
+NombreÑ Rosalinda Aquino Pérez
+Fecha: 04/04/2025
+Instrucciones:
+Recibirá una matriz a y un valor x. Solo necesita comprobar si la matriz proporcionada contiene el valor.
+a Puede contener números o cadenas. x Puede ser cualquiera de los dos.
+Devuelve true si la matriz contiene el valor, false si no.
 """
-def check(seq, elem):
-    for i in range(len(seq)):
-        if seq == elem:
-            return True
-    return False
+def check(seq:list[int], elem:int)->bool:
+    #Parametros : seq: Matriz de numeros
+    #elem: Numero ingresado
+    for i in seq:#Recorre la lista
+        if i == elem:#Si encuentra el elemento
+            return True#devuelve true
+    return False#caso contrario devuelve false
+if __name__ == "__main__":
+    seq=[1,4,5,6,2,5]
+    elem=int(input("Ingresa un número:"))
+    print(check(seq,elem))
