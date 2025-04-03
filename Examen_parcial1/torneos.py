382eb2928a9f2de12928e50562c4ca26db2555d2
rosa <perezvelayuo@gmail.com>
2025-04-02 22:12:12 -0600

    por terminar..

--- Examen_parcial1/torneos.py
+++ Examen_parcial1/torneos.py
@@ -4,6 +4,7 @@ class Torneos:
     def __init__(self,nombre:str,*equipos:tuple[Equipo]):
         self._nombre=nombre
         self._equipos=list(equipos)
+
     def agregar_equipos(self,*equipos: tuple[Equipo])->None:
         for equipo in list(equipos):
             self._equipos.append(equipo)
@@ -15,7 +16,6 @@ class Torneos:
             else:
                 print(f"El empleado {equipo} no forma parte de {self._equipos}")
 
-
     def mostrar_equipos(self,*equipos: tuple[Equipo]):
         for equipo in self._equipos:
             print(equipo)
@@ -23,5 +23,10 @@ class Torneos:
     def generar_rol(self):
         pass
     def __str__(self):
-        def __str__(self):
-            return f"Jugador(Nombre del jugador:{self._nombre}, numero de jugador:{self._numero},cantidad de goles anotados {self._goles})"
+        return f"Jugador(torneo:{self._nombre},equipos:{self._equipos})"
+if __name__=="__main__":
+    equipo_1=Equipo("los tigres")
+    equipo2=Equipo("los halcones")
+    Torneos=Torneos("invencible", equipo_1,equipo2)
+    print(Torneos)
+    Torneos.mostrar_equipos()
