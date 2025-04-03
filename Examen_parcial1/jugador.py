a5d8b8d25ba8571410979a5018fcb875f041d70b
rosa <perezvelayuo@gmail.com>
2025-04-02 22:19:04 -0600

    por terminar..

---
+++ Examen_parcial1/jugador.py
@@ -0,0 +1,21 @@
+class Jugador:
+    #nombre,numero,goles:Métodos de acceso para obtener y modificar atributos.
+    def __init__(self,nombre:str,numero,goles:int=0) -> None:
+        """Constructor para inicializar los atributos."""
+        self._nombre=nombre
+        self._numero=numero
+        self._goles=goles
+
+    @property
+    def nombre(self):
+        return self._nombre
+    @property
+    def numero(self):
+        return self._numero
+    @property
+    def goles(self):
+        return self._goles
+
+    def __str__(self):
+       return f"Jugador(Nombre del jugador:{self._nombre}, numero de jugador:{self._numero},cantidad de goles anotados {self._goles})"
+
