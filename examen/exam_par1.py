c02221c38479fedb8b8e960e561a350df3375ddc
rosa <perezvelayuo@gmail.com>
2025-04-02 20:39:11 -0600

    terminado..

--- examen/exam_par1.py
+++ examen/exam_par1.py
@@ -5,58 +5,75 @@ class Jugador:
         self._nombre=nombre
         self._numero=numero
         self._goles=goles
+    def nombre(self):
+        return self._nombre
 
     def anotar_goles(self,no_goles: int):
-        no_goles=self._goles
-        return self._goles
+        self._goles += no_goles
+        return no_goles
 
     def __str__(self):
        return f"Jugador(Nombre del jugador:{self._nombre}, numero de jugador:{self._numero},cantidad de goles anotados {self._goles})"
 
-_id:int
-_nombre:str
-_jugadores:list[Jugador]
-_id_equipo:int{id_equipo=_id}
-
+_id = 0
 class Equipo:
-    contador_id:int=0
-    no_id = 1
     def __init__(self,nombre:str,*jugadores:tuple[Jugador]):
+        """Constructor de la clase equipo, que representa un equipo"""
+        self._id_equipo=Equipo._id
         self._nombre=nombre
-        self._jugadores=tuple[jugadores]
+        self._jugadores=list(jugadores)
+        Equipo._id+=1
 
-    def agregar_jugadores(self,*jugadores: tuple[Jugador]):
+    def nombre(self):
+        return self._nombre
+
+    def agregar_jugadores(self,*jugadores: tuple[Jugador])->None:
+        """Se utiliza para agregar jugadores"""
         for jugador in jugadores:
-            if jugador != self._jugadores:
-                jugadores=jugadores.append(jugador)
-            else:
-                print("El jugador ya existe")
-        return self._jugadores
+            self._jugadores.append(jugador)
 
-    def remover_jugadores(self,*jugadores: tuple[Jugador]):
-        pass
+    def remover_jugadores(self,*jugadores: tuple[Jugador])->None:
+        """Se utilizapara remover un jugador del equipo """
+        for jugador in jugadores:
+            if jugador in self._jugadores:
+                self._jugadores.remove(jugador)
+            else:
+                print(f"El {jugador} no forma parte de {self._nombre}.")
+
     def mostrar_jugadores(self):
-        pass
+        for jugador in self._jugadores:
+            print(jugador)
+
     def total_goles(self):
-        pass
+        return sum(jugador._goles for jugador in self._jugadores)
+
     def __str__(self):
-        pass
-_nombre:str
-_equipos:list[Equipo]
+        def __str__(self):
+            return f"Jugador(Nombre del jugador:{self._nombre}, numero de jugador:{self._numero},cantidad de goles anotados {self._goles})"
+
 class Torneos:
     def __init__(self,nombre:str,*equipos:tuple[Equipo]):
         self._nombre=nombre
-        self._equipos=equipos
-    def agregar_equipos(self,*equipos: tuple[Equipo]):
-        pass
-    def remover_equipos(self,*equipos: tuple[Equipo]):
-        pass
+        self._equipos=list(equipos)
+    def agregar_equipos(self,*equipos: tuple[Equipo])->None:
+        for equipo in equipos:
+            self._equipos.append(equipo)
+
+    def remover_equipos(self,*equipos: tuple[Equipo])->None:
+        for equipo in equipos:
+            if equipo in self._equipos:
+                self._equipos.remove(equipo)
+            else:
+                print(f"El empleado {equipo} no forma parte de {self._equipos}")
+
     def mostrar_equipos(self,*equipos: tuple[Equipo]):
-        pass
+        for equipo in self._equipos:
+            print(equipo)
+
     def generar_rol(self):
         pass
     def __str__(self):
-        pass
+        return f"Jugador(Nombre del jugador:{self._nombre}, numero de jugador:{self._numero},cantidad de goles anotados {self._goles})"
 
 if __name__=="__main__":
     print("Bienvenido al torneo: Champions ")
@@ -75,5 +92,4 @@ if __name__=="__main__":
         print("[11].-Anotar goles a un jugador.")
         print("[0].-Salir.")
         op=int(input("Ingresa una opción"))
-        if op == 1:
-            agre
+
