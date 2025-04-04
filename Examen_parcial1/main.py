05cd6d1b2fb75432e125215d508615f7205bd9e9
rosa <perezvelayuo@gmail.com>
2025-04-04 10:04:37 -0600

    por terminar..

--- Examen_parcial1/main.py
+++ Examen_parcial1/main.py
@@ -1,7 +1,10 @@
 from jugador_clase import Jugador
 from equipo import Equipo
+from torneos import Torneo
 
 def menu():
+    jugadores = []
+    list_equipo=[]
     opcion=None
     while opcion != 0:
         print("Menú Principal")
@@ -23,52 +26,107 @@ def menu():
             print("Saliendo")
             break
         elif opcion == 1:
-            jugadores=[]
-            nombre = input("Ingresa el nombre del jugador: ")
-            numero = int(input("Ingresa el número del jugador: "))
-            goles = int(input("Ingresa la cantidad de goles: "))
-            while bool(nombre) and bool(numero) and bool(goles):
-                jugadores.append(Jugador(nombre, numero,goles))
-                print(f"Jugador {nombre} creado exitosamente.")
-                break
-            else:
-                print("Datos inválidos, por favor intenta nuevamente.")
-
+            crear_jugador(jugadores)
         elif opcion == 2:
-            list_equipo=[]
-            nombre = input("Ingresa el nombre del equipo.")
-            if nombre:
-                list_equipo.append(Equipo(nombre,jugadores))
+            crear_equipo(list_equipo,*jugadores)
         elif opcion==3:
-            if jugadores:
-                print("Lista de jugadores:")
-                for jugador in jugadores:
-                    print(jugador)
-            else:
-                print("No hay jugadores registrados.")
+            mostrar_lista_jugadores(jugadores)
         elif opcion==4:
-            if list_equipo:
-                print("Lista de equipos:")
-                for equipo in list_equipo:
-                    print(equipo)
-            else:
-                print("No hay equipos registrados.")
+            mostrar_lista_equipos(list_equipo)
         elif opcion==5:
-            pass
+          agregar_jugadores(jugadores,list_equipo)
         elif opcion==6:
-            pass
+            eliminar_jugadores()
         elif opcion==7:
-            pass
+            agregar_equipos_torneo(list_equipo)
         elif opcion==8:
-            pass
+           eliminar_equipos_del_torneo()
         elif opcion==9:
-            pass
+            anotar_goles_a_un_jugador()
         elif opcion==10:
-            pass
+            total_de_goles()
         elif opcion==11:
-            pass
+            generarl_rol()
 
 
+def crear_jugador(jugadores):
+    nombre = input("Ingresa el nombre del jugador: ")
+    numero = int(input("Ingresa el número del jugador: "))
+    goles = int(input("Ingresa la cantidad de goles: "))
+    if nombre.strip() and numero > 0 and goles >= 0:
+        jugadores.append(Jugador(nombre, numero, goles))
+        print(f"Jugador {nombre} creado exitosamente.")
+    else:
+        print("Datos inválidos, por favor verifica que el número y los goles sean positivos.")
+
+def crear_equipo(list_equipo,*jugadores):
+    while True:
+        nombre = input("Ingresa el nombre del equipo o '1' para terminar: ")
+        if nombre == "1":
+            break
+        equipo = Equipo(nombre, *jugadores)
+        list_equipo.append(equipo)
+def mostrar_lista_jugadores(jugadores):
+    if jugadores:
+        print("Lista de jugadores:")
+        for jugador in jugadores:
+            print(jugador)
+    else:
+        print("No hay jugadores registrados.")
+
+def mostrar_lista_equipos(list_equipo):
+    if list_equipo:
+        print("Lista de equipos:")
+        for equipo in list_equipo:
+            print(equipo)
+    else:
+        print("No hay equipos registrados.")
+
+def agregar_jugadores(jugadores, list_equipo):
+    crear_jugador(jugadores)
+    nombre_jugador = input("Escribe el nombre del jugador: ")
+    jugador_seleccionado = " "
+    for jugador in jugadores:
+        if jugador.nombre == nombre_jugador:
+            nuevo = jugador
+            break
+    if jugador_seleccionado:
+        print(f"Jugador {nuevo.nombre}.")
+        return nuevo
+    else:
+        print(f"No se encontró un jugador con el nombre '{nombre_jugador}'.")
+
+def agregar_equipos_torneo(list_equipo,*jugadores):
+        print("ingresa el nombre del equipo:")
+        for equipo in list_equipo:
+            if equipo != list_equipo:
+                print("No hay equipos disponibles.")
+            else:
+                crear_equipo(list_equipo,*jugadores)
+def eliminar_jugadores():
+    pass
+def agregar_equipos_al_torneo():
+    pass
+def eliminar_equipos_del_torneo():
+    pass
+def anotar_goles_a_un_jugador():
+    pass
+
+def total_de_goles(jugadores):
+    for i in jugadores:
+        if i!= jugadores:
+            return 0
+        return sum(jugador.goles for jugador in jugadores)
+
+def generarl_rol(equipos):
+    if len(equipos) < 2:
+        print("No hay suficientes equipos.")
+        return " "
+    jornadas = []
+    for equipo1 in enumerate(equipos):
+        for equipo2 in enumerate(equipos):
+            if equipo1 < equipo2:
+                jornadas.append((equipo1, equipo2))
+    return jornadas
 if __name__ == "__main__":
    menu()
-
