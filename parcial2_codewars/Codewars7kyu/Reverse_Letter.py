0eb91e074df4f1c9ac32bdddee64b2a8ce14df2f
rosa <perezvelayuo@gmail.com>
2025-05-13 22:21:40 -0600

    parcial 2

--- parcial2_codewars/Codewars7kyu/Reverse_Letter.py
+++ parcial2_codewars/Codewars7kyu/Reverse_Letter.py
@@ -1,3 +1,15 @@
+"""
+Tarea
+Dada una cadena str, inviértala y omita todos los caracteres no alfabéticos.
+Ejemplo
+Para str = "krishan", la salida debe ser "nahsirk".
+
+Para str = "ultr53o?n", la salida debe ser "nortlu".
+Entrada/Salida
+[input]cadenastr
+Una cadena consta de letras latinas minúsculas, dígitos y símbolos.
+[output]una cuerda
+"""
 def reverse_letter(st):
     letras = ""
 
