a828ae4888143ee5b7747a33a3ddfcdf8a6dbe1f
rosa <perezvelayuo@gmail.com>
2025-05-13 22:06:28 -0600

    parcial 2

--- parcial2_codewars/Codewars7kyu/print_count_and_numbers.py
+++ parcial2_codewars/Codewars7kyu/print_count_and_numbers.py
@@ -1,19 +1,12 @@
-data = "112233"
-
-
 def count_me(data):
     contador = {}
     resultado = ""
     contados = ""
-
-    # Contar ocurrencias
     for i in data:
         if i in contador:
             contador[i] += 1
         else:
             contador[i] = 1
-
-    # Construir el resultado en orden de aparición
     for i in data:
         if i not in contados:
             resultado += str(contador[i]) + i
@@ -21,7 +14,7 @@ def count_me(data):
 
     return resultado
 
-
+data = "112233"
 print(count_me(data))
 
 
