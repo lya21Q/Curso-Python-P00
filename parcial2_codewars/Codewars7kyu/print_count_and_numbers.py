7f06121f59345f7d416486883a70d4aac6e3c2bf
rosa <perezvelayuo@gmail.com>
2025-05-13 22:34:47 -0600

    parcial 2

--- parcial2_codewars/Codewars7kyu/print_count_and_numbers.py
+++ parcial2_codewars/Codewars7kyu/print_count_and_numbers.py
@@ -1,3 +1,17 @@
+"""
+Dada una cadena de números enteros, cuente cuántas veces se repite ese número entero y luego devuelva una cadena que muestre el recuento y el número entero.
+
+Ejemplo:"1123"
+
+Aquí 1 viene dos veces así <count><integer>será"21"
+entonces 2 viene una vez así <count><integer>será"12"
+entonces viene 3 una vez así <count><integer>será"13"
+Por lo tanto, la cadena de salida será "211213".
+
+De manera similar "211213"regresará "1221121113" (1 vez 2, 2 veces 1, 1 vez 2, 1 vez 1, 1 vez 3)
+
+Retorna :""para cadenas vacías, nulas o no numéricas
+"""
 def count_me(data):
     contador = {}
     resultado = ""
