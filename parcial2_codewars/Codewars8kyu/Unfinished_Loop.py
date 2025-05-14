d315ee1b7f78e0d67a8279ecda31f23392caa918
rosa <perezvelayuo@gmail.com>
2025-05-13 23:21:18 -0600

    Parcial 2

--- parcial2_codewars/Codewars8kyu/Unfinished_Loop.py
+++ parcial2_codewars/Codewars8kyu/Unfinished_Loop.py
@@ -1,3 +1,9 @@
+"""
+Dada una matriz de números enteros, devuelve una nueva matriz con cada valor duplicado.
+Por ejemplo:
+[1, 2, 3] --> [2, 4, 6]
+"""
+
 def cadena_a_flotante(cadena):
     no_puntos = cadena.count(".")
     no_guiones = cadena.count("-")
@@ -7,6 +13,8 @@ def cadena_a_flotante(cadena):
         return float(cadena)
     else:
         return None
-num_cadena = input("Ingresa un número flotante: ")
-numero = cadena_a_flotante(num_cadena)
-print(f"El número convertido a float es: {numero}")
+
+if __name__=="__main__":
+    num_cadena = input("Ingresa un número flotante: ")
+    numero = cadena_a_flotante(num_cadena)
+    print(f"El número convertido a float es: {numero}")
