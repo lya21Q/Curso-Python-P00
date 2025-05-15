be16fc6bff3c98df3660cf10deea6c7298fa4cf5
rosa <perezvelayuo@gmail.com>
2025-05-14 18:17:59 -0600

    Parcial 2

--- parcial2_codewars/codewars6kyu/encuentra_el_numero_unico.py
+++ parcial2_codewars/codewars6kyu/encuentra_el_numero_unico.py
@@ -1,8 +1,18 @@
+"""
+Hay una matriz con algunos números.
+Todos son iguales excepto uno. ¡Intenta encontrarlo!
+"""
 def find_uniq(arr):
-    if arr[0] == arr[1]:    # Comprobamos los primeros tres elementos de arr[i] para determinar cuál es el repetido
+    if arr[0] == arr[1]:
+        base = arr[0]
+    elif arr[0] == arr[2]:
         base = arr[0]
     else:
-        base = arr[2] if arr[0] == arr[2] else arr[0]
-    for n in arr:    # Recorremos la lista para encontrar el número diferente
+        base = arr[2]
+
+    for n in arr:
         if n != base:
             return n
+if __name__=="__main__":
+    arr=[ 1, 1, 1, 2, 1, 1 ]
+    print(find_uniq(arr))
